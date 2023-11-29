048305692f2dc8efac3629d02947a289a81e28cb
asif580244 <asifislam580244@gmail.com>
2023-11-29 22:17:11 +0800

    this is all file added

---
+++ hasset.dart
@@ -0,0 +1,13 @@
+
+
+
+import 'dart:collection';
+
+void main(){
+
+   var hashset=new HashSet();
+   hashset.addAll( ["ariful","29","somon"]);
+   hashset.remove( "somon");
+
+  print( hashset);
+}
